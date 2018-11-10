7c7a625b2c6b28e305c796e97a3ab8e916b379b6
Marcos Aidos <30325304+AidosMarcos@users.noreply.github.com>
2018-11-10 15:04:21 +0000

    new section, social network

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     $(window).scroll(function() {
-        if($(this).scrollTop() > 170) {
+        if($(this).scrollTop() > 150) {
             $('.navbar').addClass('solid');
             $('.fa-bars').addClass('toggler-black');
         } 
@@ -10,6 +10,22 @@ $(document).ready(function() {
         }
     });
 });
+
+// Smooth Scroll
+let anchorlinks = document.querySelectorAll('a[href^="#"]')
+ 
+for (let item of anchorlinks) { 
+    item.addEventListener('click', (e)=> {
+        let hashval = item.getAttribute('href')
+        let target = document.querySelector(hashval)
+        target.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        })
+        history.pushState(null, null, hashval)
+        e.preventDefault()
+    })
+}
 
 var typed2 = new Typed('#typed-text', {
     strings: ['A WEB DEVELOPER', 'A TECNOLOGY ENTHUSIAST', 'A COFFEE ADDICT', 'A BOARDGAMES LOVER'],
